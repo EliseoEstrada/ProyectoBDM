d54bb0ec70dfcbe5207f98ec5e92616cfa00ccb1
EliseoEstrada <47023970+EliseoEstrada@users.noreply.github.com>
2021-03-08 01:04:45 -0600

    Diseño de pagina final
    HTMLs completos

--- js/functions.js
+++ js/functions.js
@@ -1,16 +1,4 @@
-////////////////LOGIN/////////////////////////
-function hideLogin() {
-    var x = document.getElementById("logIn");
-    var y = document.getElementById("signUp");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-        y.style.display = "none";
-    } else {
-        x.style.display = "none";
-        y.style.display = "block";
-    }
-}
-
+////////////////GLOBAL/////////////////////////
 function previewImage(event){
     var reader = new FileReader();
     var imageField = document.getElementById("image-field")
@@ -24,23 +12,42 @@ function previewImage(event){
     reader.readAsDataURL(event.target.files[0]);
 }
 
+////////////////LOGIN/////////////////////////
+function hideLogin() {
+    var x = document.getElementById("logIn");
+    var y = document.getElementById("signUp");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        y.style.display = "none";
+    } else {
+        x.style.display = "none";
+        y.style.display = "block";
+    }
+}
+
 function validarContraseña(){
     var inputPassword = document.getElementById("password").value;
     var invalid = document.getElementsByClassName('invalid')[0];
 
     invalid.style.display = "none";
 
+    //Minimo 8 caracteres
     if (inputPassword.length < 8) {
         invalid.style.display = "block";
         invalid.innerHTML = "Debe de haber almenos 8 caracteres";
         return false;
     }
     
+    var mayusculas="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var numeros="0123456789";
     var caracteres="@¡”#$%&/=’?¡¿:;,.-_+*{][}";
+    var tieneMayuscula = false;
     var tieneNumeros = false;
     var tieneCaracteres = false;
     for(i = 0; i < inputPassword.length; i++){
+        if (mayusculas.indexOf(inputPassword.charAt(i),0) != -1){        
+            tieneMayuscula = true;
+        }
         if (numeros.indexOf(inputPassword.charAt(i),0) != -1){        
             tieneNumeros = true;
         }
@@ -49,14 +56,22 @@ function validarContraseña(){
         }
     }
 
+    //Caracter numerico
+    if(tieneMayuscula == false){
+        invalid.style.display = "block";
+        invalid.innerHTML = "Debe de haber almenos una mayuscula";
+        return false;
+    }
+    //Caracter numerico
     if(tieneNumeros == false){
         invalid.style.display = "block";
         invalid.innerHTML = "Debe de haber almenos un caracter numerico";
         return false;
     }
+    //Caracter especial
     if(tieneCaracteres == false){
         invalid.style.display = "block";
-        invalid.innerHTML = "Debe de haber un caracter especial";
+        invalid.innerHTML = "Debe de haber un caracter especial: @¡”#$%&/=’?¡¿:;,.-_+*{][}";
         return false;
     }
 
@@ -64,10 +79,51 @@ function validarContraseña(){
 }
 
 
-/////////////////ADDPRODUCT
+/////////////////ADDCOURSE
 $(".custom-file-input").on("change", function() {
     var fileName = $(this).val().split("\\").pop();
     $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+});
+
+function hideNewCategory(option){
+    
+    var x = document.getElementById("newCategory");
+    var y = document.getElementById("labelNewCategory");
+
+    if(option.value == 0){
+        x.style.display = "block";
+        x.required = true;
+        y.style.display = "block";
+        
+    }else{
+        x.style.display = "none";
+        x.required = false;
+
+        y.style.display = "none";
+
+    }
+}
+
+function changeTextTypePay(option){
+    var x = document.getElementById("labelPrice");
+    if(option.value == 0){
+        x.innerHTML= 'Precio por curso completo'; 
+        
+    }else{
+        x.innerHTML = "Precio por capitulo";
+    }
+}
+
+//ADD CHAPTERS
+//Agregar lista por cada recurso
+$('#customFile2').on('change', function(){
+    var lista = document.getElementById("resources");
+    var elemento = document.getElementById('customFile2').files[0].name;
+    
+    var li = document.createElement("li");
+    li.textContent = elemento;
+
+    lista.appendChild(li);
 });
 
 //////////////////BUY COURSE
